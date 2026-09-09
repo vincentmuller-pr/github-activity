e5c116c18ad1011d20268254692e92d20867bad1
Vincent Müller <vincent.muller.pr@gmail.com>
2026-09-09 18:29:26 -0300

    feat: implementar el resto de cases

--- src/index.js
+++ src/index.js
@@ -26,26 +26,35 @@ async function main() {
                     console.log(`- ${gevent.payload.pages.length} pages created/updated in ${gevent.repo.name}`)
                     break;
                 case "IssueCommentEvent":
+                    console.log(`- issue/PR comment ${gevent.payload.action} in ${gevent.repo.name}`)
                     break;
                 case "IssuesEvent":
+                    console.log(`- ${gevent.payload.action} issue in ${gevent.repo.name}`)
                     break;
                 case "MemberEvent":
+                    console.log(`- ${gevent.payload.action} to ${gevent.repo.name}`)
                     break;
                 case "PublicEvent":
+                    console.log(`- made ${gevent.repo.name} public`)
                     break;
                 case "PullRequestEvent":
+                    console.log(`- pull request ${gevent.payload.action} in ${gevent.repo.name}`)
                     break;
                 case "PullRequestReviewEvent":
+                    console.log(`- pull request review ${gevent.payload.action} in ${gevent.repo.name}`)
                     break;
                 case "PullRequestReviewCommentEvent":
+                    console.log(`- pull request review comment ${gevent.payload.action} in ${gevent.repo.name}`)
                     break;
                 case "PushEvent":
                     const total_commits = await fetch_push_commits(gevent.repo.url, gevent.payload.head, gevent.payload.before);
                     console.log(`- pushed ${total_commits} commits to ${gevent.repo.name}`)
                     break;
                 case "ReleaseEvent":
+                    console.log(`- ${gevent.payload.action} release in ${gevent.repo.name}`)
                     break;
                 case "WatchEvent":
+                    console.log(`- stared ${gevent.repo.name}`)
                     break;
             }
         }
