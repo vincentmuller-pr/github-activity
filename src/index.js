24e83262114bea792e02835dc1f3194172a71983
Vincent Müller <vincent.muller.pr@gmail.com>
2026-09-09 18:17:15 -0300

    feat: discussion, fork, gollum events cases

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ async function main() {
         for (gevent of gevent_list) {
             switch(gevent.type) {
                 case "CommitCommentEvent":
+                    console.log(`- commit comment ${gevent.payload.action} in ${gevent.repo.name}`)
                     break;
                 case "CreateEvent":
                     console.log(`- ${gevent.payload.ref_type} created in ${gevent.repo.name} `)
@@ -16,10 +17,13 @@ async function main() {
                     console.log(`- ${gevent.payload.ref_type} deleted in ${gevent.repo.name} `)
                     break;
                 case "DiscussionEvent":
+                    console.log(`- discussion ${gevent.payload.action} in ${gevent.repo.name}`)
                     break;
                 case "ForkEvent":
+                    console.log(`- forked ${gevent.repo.name}`)
                     break;
                 case "GollumEvent":
+                    console.log(`- ${gevent.payload.pages.length} pages created/updated in ${gevent.repo.name}`)
                     break;
                 case "IssueCommentEvent":
                     break;
